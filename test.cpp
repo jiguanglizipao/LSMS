94e10593701a28a8228bbdb171e93c08aa91a5c6
王赢绪 <wangyingxu1995@foxmail.com>
2015-09-24 14:15:48 +0800

    test stash

---
+++ test.cpp
@@ -0,0 +1,35 @@
+//#include <cmath>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+inline int getint()
+{
+	int res=0;
+	char t=getchar();
+	for (;t>'9'||t<'0';t=getchar());
+	for (;t>='0'&&t<='9';t=getchar()) res=res*10-'0'+t;
+	return res;
+}
+
+int main()
+{
+	freopen ("a.in","r",stdin);
+	
+
+
+
+	
+	
+	
+	
+	return 0;
+}
+
