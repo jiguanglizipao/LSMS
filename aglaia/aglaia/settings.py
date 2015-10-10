de577f78dc5b03ff4e456e556173807bc8ad432e
onionyst <onionyst@126.com>
2015-10-10 14:50:38 +0800

    Modified apply button style in goods_list.html. Added delete-property-button and such logic.

--- aglaia/aglaia/settings.py
+++ aglaia/aglaia/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'aglaia',
 		'USER': 'root',
-		'PASSWORD': 'root',
+		'PASSWORD': '',
 		'HOST': '127.0.0.1',
 		'PORT': '3306',
 	}
