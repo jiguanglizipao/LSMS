078344f992a529c830558d4f43dcdbfd692254e2
冯冠宇 <jiguanglizipao@163.com>
2015-11-19 01:10:06 +0800

    修改上次提交中消息中心相关的测试代码 	修改:         aglaia/test.py

--- aglaia/aglaia/test.py
+++ aglaia/aglaia/test.py
@@ -7,7 +7,11 @@ class MessageCenterTestCase(TestCase):
 
     def setUp(self):
         self.message = Message(max_num=5)
-        self.assertEqual(self.message.getTime() <= time.strftime(
+        self.assertEqual(self.message.getTime()['Recv_Readed_Time'] < time.strftime(
+            '%Y-%m-%d %H:%M:%S',
+            time.localtime(
+                time.time())), True)
+        self.assertEqual(self.message.getTime()['Send_Readed_Time'] < time.strftime(
             '%Y-%m-%d %H:%M:%S',
             time.localtime(
                 time.time())), True)
